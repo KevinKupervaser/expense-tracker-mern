45b44b838980a8c15cb800b2450b64f345b86c46
kevinkupervaser <kevinkupervaser@gmail.com>
2022-08-12 15:54:24 -0300

    initial

--- dbConnect.js
+++ dbConnect.js
@@ -1,16 +1,9 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
+const mongoose = require('mongoose')
 
-mongoose.connect(
-  "mongodb+srv://kevinkupervaser:martinembon1992@cluster0.2ltvv.mongodb.net/money-tracker-app",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect('mongodb+srv://sathya:sathyapr@cluster0.wrqpt.mongodb.net/sheymoney-udemy' , {useNewUrlParser : true , useUnifiedTopology : true})
 
-const connection = mongoose.connection;
+const connection = mongoose.connection
 
-connection.on("error", (err) => console.log(err));
+connection.on('error', err => console.log(err))
 
-connection.on("connected", () => console.log("mongo db connection succesfull"));
+connection.on('connected' , () => console.log('Mongo DB Connection Successfull'))
