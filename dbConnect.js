025981dc940bc17123d473eec6d0853ba6b1b01b
kevinkupervaser <kevinkupervaser@gmail.com>
2022-08-12 15:59:39 -0300

    initial

--- dbConnect.js
+++ dbConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://sathya:sathyapr@cluster0.wrqpt.mongodb.net/sheymoney-udemy' , {useNewUrlParser : true , useUnifiedTopology : true})
+mongoose.connect("mongodb+srv://kevinkupervaser:martinembon1992@cluster0.2ltvv.mongodb.net/money-tracker-app" , {useNewUrlParser : true , useUnifiedTopology : true})
 
 const connection = mongoose.connection
 
