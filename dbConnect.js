ffc8bddb83a571840276010353716431cc0675f7
kevinkupervaser <kevinkupervaser@gmail.com>
2022-08-12 16:24:13 -0300

    initial

--- dbConnect.js
+++ dbConnect.js
@@ -1,9 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://kevinkupervaser:martinembon1992@cluster0.2ltvv.mongodb.net/money-tracker-app" , {useNewUrlParser : true , useUnifiedTopology : true})
+mongoose.connect(
+  "mongodb+srv://kevingregori1992:martinembon1992@cluster01.nfwsyqf.mongodb.net/moneyapp",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
-const connection = mongoose.connection
+const connection = mongoose.connection;
 
-connection.on('error', err => console.log(err))
+connection.on("error", (err) => console.log(err));
 
-connection.on('connected' , () => console.log('Mongo DB Connection Successfull'))
+connection.on("connected", () =>
+  console.log("Mongo DB Connection Successfull")
+);
