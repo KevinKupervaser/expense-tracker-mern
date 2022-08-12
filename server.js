7ed40f6dd6845c6ae342296bba42643bcf161686
kevinkupervaser <kevinkupervaser@gmail.com>
2022-08-12 14:08:47 -0300

    initial

--- server.js
+++ server.js
@@ -10,15 +10,8 @@ require("dotenv").config();
 app.use("/api/users/", userRoute);
 app.use("/api/transactions/", transactionRoutes);
 
-// app.use(express.static(path.join(__dirname, "/client/build")));
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "/client/build", "index.html"));
-// });
 
 const port = process.PORT || 5000;
-
-// const port = 'https://moneytrackerapp-01.herokuapp.com/login'
 
 if (process.env.NODE_ENV === "production") {
   app.use("/", express.static("client/build"));
